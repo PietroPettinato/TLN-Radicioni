8bf40c03f97df5b6bbcb7be3b53af15f7b6da78f
pietro <pietro.pettinato@edu.unito.it>
2021-11-01 20:54:44 +0100

    fixed lowest common subsumer

--- 1 - WSD/1-similarity.py
+++ 1 - WSD/1-similarity.py
@@ -36,39 +36,88 @@ def bck_lowest_common_subsumer(sense1, sense2):
     return ancestors[max([ancestors.index(i) for i in res])]
 
 
+def bck2_lowest_common_subsumer(sense1, sense2):
+    ancestors1 = get_all_ancestors(sense1)
+    print('anc1: ', ancestors1)
+    ancestors2 = get_all_ancestors(sense2)
+    print('anc2: ', ancestors2)
+    inters = [a for a in ancestors1.keys() if a in ancestors2.keys()]
+    print("inters: ", inters)
+    if len(inters) == 0:
+        return None
+    max = 0
+    lcs = None
+    for s in inters:
+        path_len = 0
+        k = [s]
+        while str(k) != '':
+            k = ancestors1[k[0]]
+            path_len += 1
+        if path_len > max:
+            lcs = s
+            max = path_len
+    return lcs
+
+
 def lowest_common_subsumer(sense1, sense2):
-    ancestors = get_all_ancestors(sense1)
-    # print('anc1: ', ancestors)
+    ancestors1 = get_all_ancestors(sense1)
+    # print('anc1: ', ancestors1)
     ancestors2 = get_all_ancestors(sense2)
     # print('anc2: ', ancestors2)
-    res = list(set(ancestors).intersection(ancestors2))
-    print("res: ", res)
-    if len(res) == 0:
+    inters = [a for a in ancestors1.keys() if a in ancestors2.keys()]
+    # print("inters: ", inters)
+    if len(inters) == 0:
         return None
-    '''
-    if sense1.lowest_common_hypernyms(sense2)[0] != ancestors[max([ancestors.index(i) for i in res])]:
-        print("\n\n--------------- ERROR: lowest_common_subsumer is not the right one")
-        print("right: ", sense1.lowest_common_hypernyms(sense2))
-        print("mine : ", ancestors[max([ancestors.index(i) for i in res])])
-        input("Press Enter to continue...")
-    '''
-    # print("lcs: ", res[-1])
-    '''
-    x = res[-1]
-    while x in res:
-        x = ancestors[x]
-        print("x: ", x)
-    '''
-    return res[-1]
+    max = 0
+    lcs = None
+    for s in inters:
+        path = get_path(s, search_max=True)
+        path_len = len(path)
+        if path_len > max:
+            lcs = s
+            max = path_len
+    return lcs
 
 
 def extract_ancestors(sense: list):
     ancestors = []
     while sense[0].hypernyms():
-        print("hyp: ", sense[0].hypernyms())
+        # print("hyp: ", sense[0].hypernyms())
         ancestors.insert(0, sense[0].hypernyms()[0])
         sense = sense[0].hypernyms()
     return ancestors
+
+
+def get_all_ancestors(s1):
+    path = {}
+    if not s1.hypernyms():
+        path[s1] = ""
+        return path
+    path[s1] = s1.hypernyms()
+    for s in s1.hypernyms():
+        path.update(get_all_ancestors(s))
+    return path
+
+
+# trova il percorso minimo fra il nodo e la radice
+def get_path(s1, search_max: bool = False):
+    path = [s1]
+    if not s1.hypernyms():
+        return path
+    count = 0
+    ancestors = []
+    for s in s1.hypernyms():
+        tmp = get_path(s, search_max)
+        if search_max:
+            if len(tmp) > count or count == 0:
+                count = len(tmp)
+                ancestors = tmp
+        else:
+            if len(tmp) < count or count == 0:
+                count = len(tmp)
+                ancestors = tmp
+    path += ancestors
+    return path
 
 
 def depth(sense):
@@ -136,10 +185,10 @@ def find_path(s1, s2):
 def sim_shortest_path(s1, s2):
     path_len = 0
     if s1.pos() == 'v':
-        p1 = extract_ancestors([syn1[i]])
+        p1 = extract_ancestors([s1])
         # p1.append(syn1[i])
-        p2 = extract_ancestors([syn2[j]])
-        p2.append(syn2[j])
+        p2 = extract_ancestors([s2])
+        p2.append(s2)
         # print("p1: ", p1)
         # print("p2: ", p2)
         path = p1 + p2
@@ -157,7 +206,7 @@ def sim_shortest_path(s1, s2):
     if path_len == 0:
         return None
     # return (2 * max_depth - path_len) / (2 * max_depth)
-    return 1/(1+path_len) # con questa formula esce lo stesso risultato della libreria
+    return 1 / (1 + path_len)  # con questa formula esce lo stesso risultato della libreria
 
 
 def sim_leakcock_chodorow(s1, s2):
@@ -175,17 +224,17 @@ def sim_leakcock_chodorow(s1, s2):
     # print("path: ", path)
     # return - log((1 + len(path)) / (1 + 2 * max_depth))
     if lcs is None and s1.pos() == 'v':
-        p1 = extract_ancestors([syn1[i]])
+        p1 = extract_ancestors([s1])
         # p1.append(syn1[i])
-        p2 = extract_ancestors([syn2[j]])
-        p2.append(syn2[j])
+        p2 = extract_ancestors([s2])
+        p2.append(s2)
         # print("p1: ", p1)
         # print("p2: ", p2)
         path = p1 + p2
-        return - log((1 + len(path) +1) / (2 * (max_depth_v +1)))
+        return - log((1 + len(path) + 1) / (2 * (max_depth_v + 1)))
     if s1.pos() == 'v':
         return - log((1 + len(path)) / (2 * max_depth_v))
-    return - log((1 + len(path)) / (2 * max_depth)) # con questa formula esce lo stesso risultato della libreria
+    return - log((1 + len(path)) / (2 * max_depth))  # con questa formula esce lo stesso risultato della libreria
     # return - log((1 + len(path)) / (1 + 2 * comp_max_depth(s1)))
 
 
@@ -205,15 +254,15 @@ def pearson_index():
 
 
 # -----------------------------------
-# -------------- TEST ---------------
+# -------------- TESTS --------------
 # -----------------------------------
 
 def check_depth(syn):
     for i in range(len(syn)):
-        #s1hp = syn[i]._shortest_hypernym_paths(False)  # simulate_root = False
-        s1hp = syn[i]._shortest_hypernym_paths(True)  # simulate_root = False
+        s1hp = syn[i]._shortest_hypernym_paths(True)
         real_depth = s1hp.get(list(s1hp)[len(s1hp) - 1])
         my_depth = depth(syn[i])
+        # my_depth = get_path(syn[i])
         if real_depth != my_depth:
             print(syn[i])
             print("my_depth  : ", my_depth)
@@ -300,67 +349,44 @@ def check_sim_wu_palmer(syn1, syn2):
                 input("ERROR: Press Enter to continue...")
                 # break
         # break
+    print("------ ok, sim_wu_palmer")
+
 
 
 def check_sim_shortest_path(syn1, syn2):
     for i in range(len(syn1)):
         for j in range(len(syn2)):
-            print("\nc1[" + str(i) + "]: ", syn1[i])
-            print("c2[" + str(j) + "]: ", syn2[j])
             right_sim = syn1[i].path_similarity(syn2[j])
             my_sim = sim_shortest_path(syn1[i], syn2[j])
-            print("Mine : ", my_sim)
-            print("Right: ", right_sim)
             if right_sim != my_sim:
+                print("\nc1[" + str(i) + "]: ", syn1[i])
+                print("c2[" + str(j) + "]: ", syn2[j])
+                print("Mine : ", my_sim)
+                print("Right: ", right_sim)
                 input("ERROR: Press Enter to continue...")
             # print("right path 1: ", syn1[i]._shortest_hypernym_paths(False))
             # print("right path 2: ", syn2[j]._shortest_hypernym_paths(False))
+    print("------ ok, sim_shortest_path")
 
 
 def check_leakcock_chodorow(syn1, syn2):
     for i in range(len(syn1)):
         for j in range(len(syn2)):
-            print("\nc1[" + str(i) + "]: ", syn1[i])
-            print("c2[" + str(j) + "]: ", syn2[j])
             my_sim = sim_leakcock_chodorow(syn1[i], syn2[j])
-            print("Mine : ", my_sim)
             try:
                 right_sim = syn1[i].lch_similarity(syn2[j])
-                print("Right: ", right_sim)
                 if right_sim != my_sim:
+                    print("\nc1[" + str(i) + "]: ", syn1[i])
+                    print("c2[" + str(j) + "]: ", syn2[j])
+                    print("Right: ", right_sim)
+                    print("Mine : ", my_sim)
                     input("ERROR: Press Enter to continue...")
             except Exception:
-                print("right sim error")
-
-
-def get_all_ancestors(s1):
-    path = {}
-    if not s1.hypernyms():
-        path[s1] = ""
-        return path
-    path[s1] = s1.hypernyms()
-    for s in s1.hypernyms():
-        path.update(get_all_ancestors(s))
-    return path
-
-
-
-# trova il percorso minimo fra il nodo e la radice
-def get_min_ancestors(s1):
-    path = [s1]
-    if not s1.hypernyms():
-        return path
-    count = 0
-    min_ancestors = []
-    for s in s1.hypernyms():
-        tmp = get_min_ancestors(s)
-        if len(tmp) < count or count == 0:
-            count = len(tmp)
-            min_ancestors = tmp
-    path += min_ancestors
-    return path
-
-
+                # print("\nc1[" + str(i) + "]: ", syn1[i])
+                # print("c2[" + str(j) + "]: ", syn2[j])
+                # print("right sim error")
+                pass
+    print("------ ok, sim_leakcock_chodorow")
 
 
 #  Word 1 | Word 2 | Human (mean)
@@ -378,28 +404,26 @@ max_depth_v = comp_max_depth('v')
 # print("max_depth_v: ", max_depth_v)
 
 
-
-w1 = 'tiger' #'love'
-w2 = 'cat' #'sex'
+# w1 = 'love'
+# w2 = 'sex'
+w1 = 'tiger'
+w2 = 'cat'
 syn1 = wn.synsets(w1)
 syn2 = wn.synsets(w2)
 # s1 = syn1[0]
 # s2 = syn2[0]
 
-print(syn1[0])
-print(syn2[0])
-print("\n\nfinale: ", lowest_common_subsumer(syn1[0], syn2[0]))
-print("correct: ", syn1[0].lowest_common_hypernyms(syn2[0]))
 
-# check_depth(syn1) # todo qui errore depth a Synset('beloved.n.01')
+
+
+# check_depth(syn1)  # todo qui errore depth a Synset('beloved.n.01')
 # check_depth(syn2)
 # check_lowest_common_subsumer(syn1, syn2)
 # check_shortest_path(syn1, syn2)
 # check_sim_wu_palmer(syn1, syn2)
 # check_leakcock_chodorow(syn1, syn2)
 # check_sim_shortest_path(syn1, syn2) # da errore ma sim_shortest_path() funziona
-input("STOP HERE")
-
+# input("STOP HERE")
 
 s1 = []
 s2 = []
@@ -429,7 +453,6 @@ for i in range(len(syn1)):
         except WordNetError:
             right_sim_lc.append(None)
 
-
 sim_df = pd.DataFrame({
     "synset 1": s1,
     "synset 2": s2,
@@ -440,7 +463,6 @@ sim_df = pd.DataFrame({
     "my_sim_lc": my_sim_lc,
     "right_sim_lc": right_sim_lc
 })
-
 
 print(sim_df.to_string())
 # trova i massimi per ogni colonna
@@ -453,7 +475,8 @@ indexes = sim_df[sim_df.columns.difference(['synset 1', 'synset 2'])].idxmax(axi
 print("\n--- riga di ogni massimo ---\n")
 print(sim_df.loc[indexes].to_string())
 
-max_df = pd.DataFrame(index=['my_sim_wp', 'right_sim_wp', 'my_sim_lc', 'right_sim_lc', 'my_sim_sp', 'right_sim_sp'], columns=['synset 1', 'synset 2', 'sim'])
+max_df = pd.DataFrame(index=['my_sim_wp', 'right_sim_wp', 'my_sim_lc', 'right_sim_lc', 'my_sim_sp', 'right_sim_sp'],
+                      columns=['synset 1', 'synset 2', 'sim'])
 
 for i, v in indexes.items():
     max_df.loc[i, 'synset 1'] = sim_df.loc[v, 'synset 1']
@@ -461,9 +484,6 @@ for i, v in indexes.items():
     max_df.loc[i, 'sim'] = sim_df.loc[v, i]
 print(max_df.to_string())
 
-
-
 # read the file
 # dataset = pd.read_csv('WordSim353.csv', header=0)
 # print(dataset.to_string())
-
