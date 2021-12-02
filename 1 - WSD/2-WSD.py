8e9315dbc222522631559c574437d1fe74825d79
pietro <pietro.pettinato@edu.unito.it>
2021-12-02 13:30:05 +0100

    added explainations to methods, cleanup

--- 1 - WSD/2-WSD.py
+++ 1 - WSD/2-WSD.py
@@ -11,18 +11,25 @@
 # "semcor", SemcorCorpusReader, r"brown./tagfiles/br-.*\.xml", wordnet
 # )  # Must be defined *after* wordnet corpus.
 
-from pprint import pprint
 from nltk.corpus import semcor as sc
 from nltk.corpus import wordnet as wn
 import numpy as np
 
 
 def lesk_algorithm(w, s):
+    """
+    Implementazione dell'algoritmo di Lesk.
+
+    :param w: la parola da disambiguare
+    :param s: la frase in cui si trova la parola w
+
+    :return: il miglior synset per la parola in base alla disambiguazione
+    """
     if not w:
         return None
     try:
         best_sense = wn.synsets(w)[0]
-    except IndexError:  # if wn.synsets(w) is empty
+    except IndexError:  # nel caso in cui wn.synsets(w) è vuoto
         best_sense = None
     max_overlap = 0
     # context = set(s.split())
@@ -42,7 +49,7 @@ def lesk_algorithm(w, s):
 
 def run():
     """
-    Esecuzione sulle prime 50 frasi del corpus e con il primo sostantivo della frase
+    Esecuzione sulle prime 50 frasi del corpus e con il primo sostantivo della frase.
 
     :return: accuracy dell'esecuzione
     """
@@ -51,64 +58,58 @@ def run():
     for s in sc.tagged_sents(tag='pos'):
         if i == 50:
             break
-        '''
-        pprint(s)
-        for t in s:
-            print(t.pos())
-            print(t.leaves())
-            print(t.label())
-            if t.label() == 'NN':
-                # w = t.pos()
-                w = t.leaves()[0]
-                print('w: ', w)
-                m = t.label()
-                print('m: ', m)
-                break
-        '''
+        # prendiamo il primo nome
         noun = [t.leaves()[0] for t in s if t.label() == 'NN'][0]
-        #print('\nnoun: ', noun)
+        # prendiamo la frase
         sentence = sc.sents()[i]
-        #print('sentence: ', sentence)
+        # usiamo l'algoritmo di Lesk per la disambiguzione del nome
         best_sense = lesk_algorithm(noun, sentence)
-        #try:
-            #print('best sense: ', best_sense.lemmas())
-        #except AttributeError:
-            #print('best sense: ', None)
 
+        # prendiamo dal corpus SemCor il senso corretto di noun
         sense_gold = None
         for t in sc.tagged_sents(tag='sem')[i]:
-            #print(t)
             if t[0] == noun:
                 try:
                     sense_gold = t.label()
                 except AttributeError:
                     pass
                 break
-        #print('correct   : ', sense_gold)
 
+        # aggiorniamo l'accuracy
         tot, corrects = accuracy
         tot += 1
-        #if best_sense and sense_gold and (best_sense.lemmas()[0] == sense_gold): # todo controllare quale if usare, il secondo è corretto o scarta anche alcune giuste?
+        # if best_sense and sense_gold and (best_sense.lemmas()[0] == sense_gold): # todo controllare quale if usare, il secondo è corretto o scarta anche alcune giuste?
         if best_sense and sense_gold and (str(best_sense.lemmas()[0]) == str(sense_gold)):
             corrects += 1
-            #print('corrects++')
-        else:
-            pass
-            #input('---- wrong ----')
+            # print('corrects++')
         accuracy = (tot, corrects)
         i += 1
     return accuracy
 
 
 def random_sentences_selector():
-    # x = 37176 # to speed up computation we use the result of len(sc.sents()) as a constant
-    x = 1500  # it takes too long with an high number (37176), so we consider a smaller fixed integer
+    """
+    Genera degli interi casuali che vengono usati come indici per scegliere frasi dal corpus SemCor.
+
+    :return: un dizionario con l'indice intero e la corrispondente frase scelta casualmente
+    """
+    # x = 37176 # per velocizzare la computazione trattiamo il risultato di len(sc.sents()) come una costante
+    x = 1500  # usando un numero molto alto (37176) la computazione è comunque troppo lenta, usiamo un intero più piccolo per velocizzare il processo
+    # questo esclude alcune frasi del corpus, ma ci consente di considerare comunque un campione abastanza vasto
+
     indexes = np.random.randint(0, x, 50)
     sentences = {i: sc.tagged_sents(tag='pos')[i] for i in indexes}
     return sentences
 
 
 def random_noun_selector(s):
+    """
+    Sceglie un nome a caso dalla frase.
+
+    :param s: frase da cui scegliere il nome
+
+    :return: il nome scelto
+    """
     nouns = [t.leaves()[0] for t in s if t.label() == 'NN']
     if len(nouns) == 0:
         return None
@@ -125,38 +126,29 @@ def randomized_run():
     accuracy = (0, 0)
     sentences = random_sentences_selector()
     for k in sentences.keys():
-        #pprint(sentences[k])
         noun = random_noun_selector(sentences[k])
-        #print('\nnoun: ', noun)
         sentence = sc.sents()[k]
-        #print('sentence: ', sentence)
         best_sense = lesk_algorithm(noun, sentence)
-        #try:
-            #print('best sense: ', best_sense.lemmas()[0])
-        #except AttributeError:
-            #print('best sense: ', None)
 
         sense_gold = None
         for t in sc.tagged_sents(tag='sem')[k]:
-            #print(t)
             if t[0] == noun:
                 try:
                     sense_gold = t.label()
                 except AttributeError:
                     pass
                 break
-        #print('correct   : ', sense_gold)
 
         tot, corrects = accuracy
         tot += 1
+        # if best_sense and sense_gold and (best_sense == sense_gold.synset()):
         if best_sense and sense_gold and (str(best_sense.lemmas()[0]) == str(sense_gold)):
             corrects += 1
-            #print('corrects++')
         accuracy = (tot, corrects)
-        #input('---- continue? ----')
     return accuracy
 
 
+# chiamiamo le funzioni e calcoliamo le accuracy
 accuracy = run()
 print('\n\n------------------------------------------------')
 print('  accuracy (tot/corrects)     : ', accuracy, end='\n')
@@ -164,7 +156,6 @@ for i in range(10):
     accuracy_rand = randomized_run()
     print(f'  {i}) accuracy rand (tot/corrects): ', accuracy_rand)
 print('------------------------------------------------')
-
 
 '''
 ------------------------------------------------
@@ -196,5 +187,7 @@ Infatti se gli indici sono troppo grandi, il tempo per l'accesso aumenta notevol
 
 L'accuracy è molto più alta per le prime 50 frasi, con la randomizzazione cala notevolmente passando da circa 50% a circa 30%, probabilmente influisce 
 il nome scelto e la complessità della frase.
+
+Il lemma scelto è sempre il primo della lista, si potrebbe provare a capire quale sia il migliore fra quelli disponibili
 
 '''
